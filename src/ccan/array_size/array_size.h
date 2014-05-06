e6c62de4c71f985fdce6b5f09e5b5011da2f2e20
Christophe Vu-Brugier <cvubrugier@yahoo.fr>
2014-05-06 21:54:59 +0200

    Fix bogus ARRAY_SIZE() macro
    I imported the ARRAY_SIZE() macro from CCAN but I changed it a little
    bit. Alas, my little changes were plain wrong because ARRAY_SIZE()
    returned SIZE + 1 :/
    
    Signed-off-by: Christophe Vu-Brugier <cvubrugier@yahoo.fr>

--- src/ccan/array_size/array_size.h
+++ src/ccan/array_size/array_size.h
@@ -7,6 +7,7 @@
 
 #ifndef CCAN_ARRAY_SIZE_H
 #define CCAN_ARRAY_SIZE_H
+#include <ccan/build_assert/build_assert.h>
 
 /**
  * ARRAY_SIZE - get the number of elements in a visible array
@@ -14,13 +15,14 @@
  *
  * This does not work on pointers, or arrays declared as [], or
  * function parameters.  With correct compiler support, such usage
- * will cause a build error.
+ * will cause a build error (see build_assert).
  */
 #define ARRAY_SIZE(arr) (sizeof(arr) / sizeof((arr)[0]) + _array_size_chk(arr))
 
 /* Two gcc extensions.
  * &a[0] degrades to a pointer: a different type from an array */
 #define _array_size_chk(arr)						\
-	!__builtin_types_compatible_p(__typeof__(arr), __typeof__(&(arr)[0]))
+	BUILD_ASSERT_OR_ZERO(!__builtin_types_compatible_p(__typeof__(arr),	\
+							   __typeof__(&(arr)[0])))
 
 #endif /* CCAN_ARRAY_SIZE_H */
