75cae5535dc1a3d6e25ad9f8d28aab9788a95cd7
Christophe Vu-Brugier <cvubrugier@fastmail.fm>
2015-03-20 14:06:05 +0100

    Add itimer_fire() and itimer_get_expiration()
    Signed-off-by: Christophe Vu-Brugier <cvubrugier@fastmail.fm>

--- src/itimer.c
+++ src/itimer.c
@@ -31,3 +31,32 @@ int itimer_stop(int fd)
 {
 	return itimer_start(fd, 0);
 }
+
+/*
+ * Change the amount of time until the timer will next expire but keep
+ * the current interval of the timer.
+ */
+int itimer_fire(int fd, unsigned int value)
+{
+	struct itimerspec old_timer, new_timer;
+
+	if (timerfd_gettime(fd, &old_timer) == -1)
+		return -1;
+	new_timer.it_value = (struct timespec) {value, 0};
+	new_timer.it_interval = old_timer.it_interval;
+
+	return timerfd_settime(fd, 0, &new_timer, NULL);
+}
+
+time_t itimer_get_expiration(int fd)
+{
+	struct itimerspec timer;
+	time_t expiration;
+
+	timerfd_gettime(fd, &timer);
+	expiration = timer.it_value.tv_sec;
+	if (timer.it_value.tv_nsec)
+		expiration += 1;
+
+	return expiration;
+}
--- src/itimer.h
+++ src/itimer.h
@@ -12,3 +12,7 @@ int itimer_create(void);
 int itimer_start(int fd, unsigned int interval);
 
 int itimer_stop(int fd);
+
+int itimer_fire(int fd, unsigned int value);
+
+time_t itimer_get_expiration(int fd);
